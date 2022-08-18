c1522cb6e0f1fcd42f19d1890d3e083025b91382
jay <jayvaghani2000@gmail.com>
2022-08-18 19:01:37 +0530

    chore

--- todo/src/app/store.js
+++ todo/src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "./../feature/post/postSlice";
 
+
 export const store = configureStore({
     reducer: {
         posts: postReducer
